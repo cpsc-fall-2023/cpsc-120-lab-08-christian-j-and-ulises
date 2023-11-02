f4c3799d434ba8f3904de64553c9408c846a7337
Flores <floresperezu7418@cs101u-5b.acad.fullerton.edu>
2023-11-01 20:04:23 -0700

    Done

--- part-2/average.cc
+++ part-2/average.cc
@@ -9,31 +9,18 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-
   if (arguments.size() <= 1) {
-    std::cerr << "Error: At least one command line argument is required."
-              << std::endl;
+    std::cout << "error: you must supply at least one number";
     return 1;
   }
-
   double sum = 0.0;
-  for (size_t i = 1; i < arguments.size(); ++i) {
-    try {
-      sum += std::stod(arguments[i]);
-    } catch (const std::invalid_argument& e) {
-      std::cerr << "Error: Invalid argument at index " << i << ": " << e.what()
-                << std::endl;
-      return 1;
-    } catch (const std::out_of_range& e) {
-      std::cerr << "Error: Argument out of range at index " << i << ": "
-                << e.what() << std::endl;
-      return 1;
+  for (std::string const& argument : arguments) {
+    if (argument == arguments[0]) {
+      continue;
     }
+    sum = sum + std::stod(argument);
   }
-
-  double average = sum / static_cast<double>(arguments.size() - 1);
-
-  std::cout << "average = " << average << std::endl;
-
+  std::cout << "average = " << sum / static_cast<double>(arguments.size() - 1)
+            << "\n";
   return 0;
 }
