c881365f918321d32fe06b7e30019b6656f5cc01
Flores <floresperezu7418@cs200u-6b.acad.fullerton.edu>
2023-11-01 18:30:00 -0700

    Finish part 2

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Ulises Flores
+// floresperezu7418@csu.fullerton.edu
+// @UlisesFloresPerez
+// Partners: @chrisjimenez36
 
 #include <iostream>
 #include <string>
@@ -7,25 +10,30 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
+  if (arguments.size() <= 1) {
+    std::cerr << "Error: At least one command line argument is required."
+              << std::endl;
+    return 1;
+  }
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
+  double sum = 0.0;
+  for (size_t i = 1; i < arguments.size(); ++i) {
+    try {
+      sum += std::stod(arguments[i]);
+    } catch (const std::invalid_argument& e) {
+      std::cerr << "Error: Invalid argument at index " << i << ": " << e.what()
+                << std::endl;
+      return 1;
+    } catch (const std::out_of_range& e) {
+      std::cerr << "Error: Argument out of range at index " << i << ": "
+                << e.what() << std::endl;
+      return 1;
+    }
+  }
 
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
+  double average = sum / static_cast<double>(arguments.size() - 1);
 
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
+  std::cout << "average = " << average << std::endl;
 
   return 0;
 }
